9ca057c9c233073cac93f7a5989bb421f72eaef8
Maxim Loginov <maxim.loginov@colruytgroup.com>
2021-09-09 17:20:34 +0200

    making skills

--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { PolarAngleAxis, PolarGrid, Radar, RadarChart, ResponsiveContainer } from 'recharts'
 import page_hoc from '../Page'
 
 const Skills = () => {
-  return <div className='content'>Skilled</div>
+  const [data, set_data] = useState([
+    {
+      subject: 'Math',
+      A: 10,
+    },
+    {
+      subject: 'Chin',
+      A: 10,
+    },
+    {
+      subject: 'Engl',
+      A: 10,
+    },
+    {
+      subject: 'Geog',
+      A: 30,
+    },
+    {
+      subject: 'Phys',
+      A: 10,
+    },
+    {
+      subject: 'Hist',
+      A: 10,
+    },
+  ])
+
+  return (
+    <div className='content'>
+      <ResponsiveContainer width='100%' height='100%'>
+        <RadarChart cx='50%' cy='50%' data={data}>
+          <PolarGrid gridType='circle' />
+          <PolarAngleAxis dataKey='subject' />
+          <Radar name='Mike' dataKey='A' fill='#ffd97d' fillOpacity={0.97} />
+        </RadarChart>
+      </ResponsiveContainer>
+    </div>
+  )
 }
 
 export default page_hoc(Skills, 'Skills')
